ffa4d502932216da46cd42a14d7bebf23c0beceb
markSancho7 <marcos.sancho7@alumnostrazos.net>
2023-11-20 01:16:38 +0100

    123

---
+++ src/js/dom.js
@@ -0,0 +1,4 @@
+export const questionQuizElement = document.getElementById('questionQuiz')
+export const containerButtons = document.getElementById('containerButtons')
+export const containerElement = document.getElementById('container')
+export const finalResultElement = document.getElementById('finalResult')
